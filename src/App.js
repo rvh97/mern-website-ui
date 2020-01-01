59b3e6073fd78b31e1fb751cfe61fa71046d226d
Roland Veiderma Holmberg <24371899+rvh97@users.noreply.github.com>
2020-01-01 21:55:29 +0100

    Added header with name and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div style={headerStyle}>
+            <h1>Name</h1>
+        </div>
+        
+    )
+}
+
+const headerStyle = {
+    color: 'white',
+    textAlign: 'center',
+    padding: '10px'
+}
+
+export default Header;
